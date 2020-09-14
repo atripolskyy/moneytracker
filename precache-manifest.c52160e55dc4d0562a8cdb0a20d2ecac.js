f9b3987f2f8345ed715e1a33de86a86e3f36e7ee
Oleksii Trypolskyi <atripolskyy@gmail.com>
2020-09-14 10:50:38 +0300

    Updates

---
+++ precache-manifest.c52160e55dc4d0562a8cdb0a20d2ecac.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83779a6f6005f1caa99af779fd6f294d",
+    "url": "/moneytracker/index.html"
+  },
+  {
+    "revision": "b0abb5d76f9eeffaf9fd",
+    "url": "/moneytracker/static/css/2.0b17412e.chunk.css"
+  },
+  {
+    "revision": "8abbda47678ae2b7d11c",
+    "url": "/moneytracker/static/css/main.9af31924.chunk.css"
+  },
+  {
+    "revision": "b0abb5d76f9eeffaf9fd",
+    "url": "/moneytracker/static/js/2.289e025b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moneytracker/static/js/2.289e025b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abbda47678ae2b7d11c",
+    "url": "/moneytracker/static/js/main.41632e3f.chunk.js"
+  },
+  {
+    "revision": "386a55bc232cf50d7856",
+    "url": "/moneytracker/static/js/runtime-main.62da18d6.js"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/moneytracker/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/moneytracker/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "851a2b5a8394eb1b868678bfd31a1a8a",
+    "url": "/moneytracker/static/media/roboto-all-500-normal.851a2b5a.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/moneytracker/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/moneytracker/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "7fd643e63fc847f47521a2604072ce9c",
+    "url": "/moneytracker/static/media/roboto-cyrillic-500-normal.7fd643e6.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/moneytracker/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/moneytracker/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "378698af3b6776fd1e3ee0a51c3d7b87",
+    "url": "/moneytracker/static/media/roboto-cyrillic-ext-500-normal.378698af.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/moneytracker/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/moneytracker/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "665639f6a0519416ad2904f1c218ea30",
+    "url": "/moneytracker/static/media/roboto-greek-500-normal.665639f6.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/moneytracker/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/moneytracker/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "0f80978b9a5916929ecd5e1e21bc4169",
+    "url": "/moneytracker/static/media/roboto-greek-ext-500-normal.0f80978b.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/moneytracker/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/moneytracker/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/moneytracker/static/media/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/moneytracker/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/moneytracker/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "b1b808436b5eafe9a683fff584d80fea",
+    "url": "/moneytracker/static/media/roboto-latin-ext-500-normal.b1b80843.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/moneytracker/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/moneytracker/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  },
+  {
+    "revision": "16423fb4da5bb6d54592839d6cd8e1be",
+    "url": "/moneytracker/static/media/roboto-vietnamese-500-normal.16423fb4.woff2"
+  }
+]);
